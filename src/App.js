e1d9904cee9c7ea9d8259ce8e3bed4ee573b2ce3
Nichole <nichole.cruzalegui@bbdo.com>
2022-09-07 16:25:18 -0400

    Adjusts imports and modularizes Routing and Navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import './App.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Logo from './img/logo.png'
-import TheWorkLink from './img/header_the_work.png'
-import TheTeamLink from './img/header_the_team.png'
-import TheCollectiveLink from './img/header_the_collective.png'
-import TalkButton from './img/header_talk_btn.png'
-
-
 import React from "react";
 import {
   BrowserRouter as Router,
-  Link
 } from "react-router-dom";
 import Routing from './components/Routing';
+import Header from './components/Header';
+
 
 
 
@@ -22,37 +13,8 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar expand="lg">
-          <Container>
-            <Navbar.Brand as={Link} to={"/"}>
-              <img id="logo" src={Logo} alt="BBDO logo" />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="justify-content-end me-auto" >
-                {/* <Nav.Link as={Link} to={"/home"}>Home</Nav.Link> */}
-                {/* <Nav.Link as={Link} to={"/"}>
-                  <img className='header-links' id="home-link" src={TheWorkLink} alt="The Home Test link" />
-                </Nav.Link> */}
-                <Nav.Link as={Link} to={"/the-work"}>
-                  <img className='header-links' id="the-work-link" src={TheWorkLink} alt="The Work link" />
-                </Nav.Link>
-                <Nav.Link as={Link} to={"/the-team"}>
-                  <img className='header-links' id="the-team-link" src={TheTeamLink} alt="The Team link" />
-                </Nav.Link>
-                <Nav.Link as={Link} to={"/the-collective"}>
-                  <img className='header-links' id="the-collective-link" src={TheCollectiveLink} alt="The Collective link" />
-                </Nav.Link>
-                <Nav.Link as={Link} to={"/talk-to-us"}>
-                  <img id="talk-to-us-button" src={TalkButton} alt="The Talk button" />
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-        
+        <Header />
         <Routing />
-        
       </Router>
     </div>
   );
