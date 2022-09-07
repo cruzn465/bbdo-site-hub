e65f83a42683d5a143d75b7372ee3be64e38bf53
Nichole <nichole.cruzalegui@bbdo.com>
2022-09-07 16:48:36 -0400

    team_fxn

--- src/components/Team.js
+++ src/components/Team.js
@@ -14,18 +14,24 @@ function Team() {
       <div id='spacer'></div>
       <Container id="test-container">
         <Image className="header" src={Team_super} alt="Who We Are"></Image>
-        <hr  />  
+        <hr />
         {/* <h2 id="team-copy-not-pic">
           WE’RE A TEAM OF AWARD-WINNING CREATORS AND UNCONVENTIONAL MAKERS, <br/>
           DELIVERING BEST-IN-CLASS CONTENT FOR GLOBAL AND LOCAL ALIKE.<br/>
           WITH QUALITY AND EFFICIENCY AT THE FOREFRONT OF EVERYTHING WE DO, WE'RE THE PEOPLE WHO ARE DRIVEN BY INNOVATION AND POWERED BY <b>THE WORK</b>
         </h2> */}
         <img className="copy" src={Team_copy} alt="WE’RE A TEAM OF AWARD-WINNING CREATORS AND UNCONVENTIONAL MAKERS" />
-        <hr />  
+        <hr />
         <Image id="team_bg" src={Team_bg_img} alt="Office Background">
           {/* <Image id="team_bg_grey" src={Team_bg_grey} alt="Office Background"></Image> */}
 
         </Image>
+
+        <Container>
+          <div id="test-image">
+            {/* <div id="test-image-grey"></div> */}
+          </div>
+        </Container>
 
       </Container>
     </Container>
