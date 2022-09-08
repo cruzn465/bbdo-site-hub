5599c791498bde90b4452de268a8e127bb2a049b
Nichole <nichole.cruzalegui@bbdo.com>
2022-09-08 15:43:37 -0400

    Places all team titles and subtitles in final place

--- src/components/Team.js
+++ src/components/Team.js
@@ -5,6 +5,13 @@ import Team_super from '../img/team_super.png'
 import Team_copy from '../img/team_copy.png'
 import Team_bg_img from '../img/team_bg_img.jpg'
 import Team_bg_grey from '../img/background_grey.png'
+import Team_prod_subcopies from '../img/prod_titles.png'
+import Team_prod from '../img/team_prod.png'
+import Team_digital_subcopies from '../img/digital_titles.png'
+import Team_digital from '../img/team_digital.png'
+import Team_post_subcopies from '../img/post_titles.png'
+import Team_post from '../img/team_post.png'
+
 
 
 
@@ -27,8 +34,17 @@ function Team() {
 
           <Image id="team_bg" src={Team_bg_img} alt="Office Background" className="img-fluid" />
           <Image id="test-image-grey" src={Team_bg_grey}></Image>
-          {/* <Image id="test-image-grey-2" src={Team_bg_grey}></Image> */}
-          {/* <Image id="test-image-grey-3" src={Team_bg_grey}></Image> */}
+          <Image id="team_prod" className="team_super_titles" src={Team_prod}></Image>
+          <Image id="team_prod_subCopies" src={Team_prod_subcopies}></Image>
+
+          <Image id="test-image-grey-2" src={Team_bg_grey}></Image>
+          <Image id="team_post" className="team_super_titles" src={Team_post}></Image>
+          <Image id="team_post_subCopies" src={Team_post_subcopies}></Image>
+
+
+          <Image id="test-image-grey-3" src={Team_bg_grey}></Image>
+          <Image id="team_digital" className="team_super_titles" src={Team_digital}></Image>
+          <Image id="team_digital_subCopies" src={Team_digital_subcopies}></Image>
 
           </figure>
 
