5f8840b6086a95f1d7e950decf38f1a5e19ad479
Nichole <nichole.cruzalegui@bbdo.com>
2022-09-19 16:16:31 -0400

    Modularizes CurrentMember component

--- src/components/Collective.js
+++ src/components/Collective.js
@@ -1,23 +1,86 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-import Image from 'react-bootstrap/Image'
-import Collective_super from '../img/collective_super.png'
-import Collective_copy from '../img/collective_copy.png'
+import { useState } from "react";
+
+import { Row, Col, Container } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
+import Collective_super from "../img/collective_super.png";
+import Collective_copy from "../img/collective_copy.png";
+import memberArr from "../members.json";
+// import Member from "./Member";
+
+// modularize into Member components
+function CurrentMember(props) {
+  // console.log("props", props);
+  const { img, name, title, pronouns } = props;
+  return (
+    <div className="flex-mem">
+      <div>
+        <img
+          id="curr-mem-img"
+          src={img}
+          className="member"
+          alt="current member"
+        />
+      </div>
+      <div id="name-title-div" className="work-sans-font">
+        {name} <br />
+        {title} | {pronouns}
+      </div>
+    </div>
+  );
+}
 
 function Collective() {
-    return (
-      <Container fluid className="bg">
-      <div id='spacer'></div>
-      <Container id="all-content">
-        <Image className="header" id="collective-header" src={Collective_super} alt="Who We Are" />
-        <hr  />  
-        <Image className="copy" src={Collective_copy} alt="WE’RE A TEAM OF AWARD-WINNING CREATORS AND UNCONVENTIONAL MAKERS" />
-        <hr />  
-        <h2 className="germain" id="curr-mem">CURRENT MEMBERS</h2>
+  const [currMem, setCurrMem] = useState(memberArr[0]);
+
+  // console.log(memberArr)
+  // <Member member={member} key={member.id} />
+  const members = memberArr.map((member) => (
+    <div key={member.id} onClick={(e) => handleMemberClick(e, member.id)}>
+      <img src={member.img} className="member" alt="individual member" />
+    </div>
+  ));
+  // click handler for when any member is clicked
+  function handleMemberClick(e, id) {
+    setCurrMem(memberArr[id - 1]);
+    console.log(memberArr[id - 1]);
+  }
+
+  // maybe use members[0] for default member
+  // let dynamicText = 'hello world'
+  return (
+    <Container fluid className="bg">
+      <div id="spacer"></div>
+      <Container className="all-content">
+        <Image
+          className="header"
+          id="collective-header"
+          src={Collective_super}
+          alt="Who We Are"
+        />
+        <hr />
+        <Image
+          className="copy"
+          src={Collective_copy}
+          alt="WE’RE A TEAM OF AWARD-WINNING CREATORS AND UNCONVENTIONAL MAKERS"
+        />
+        <hr />
+        <h2 className="germain" id="curr-mem-title">
+          CURRENT MEMBERS
+        </h2>
+        <div className="flex-mem">{members}</div>
+        <Container className="font-titles">
+          <hr />
+          <CurrentMember
+            img={currMem.img}
+            name={currMem.name}
+            title={currMem.title}
+            pronouns={currMem.pronouns}
+          />
+        </Container>
       </Container>
     </Container>
-    );
-  }
-  
-  export default Collective;
-  
+  );
+}
+
+export default Collective;
---
+++ src/components/Member.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Member({memInfo}) {
+    // const {id, name, pronouns, img} = memInfo
+    console.log("memInfo: ",memInfo)
+    // console.log(memInfo.id, name, pronouns, img)
+    return (
+        <div>This is a Member Comp</div>
+    );
+}
+
+export default Member;
