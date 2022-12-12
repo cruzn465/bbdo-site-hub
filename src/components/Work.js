43bb6aed97395970753db8ad4f2e36706badd61d
Nichole <nichole.cruzalegui@bbdo.com>
2022-12-12 17:48:56 -0500

    Uses axios to fetch data from the WP CMS

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,38 +1,18 @@
 import React from "react";
-// import { useState } from "react";
+import Axios from "axios";
+import { useState, useEffect } from "react";
 import { Container, Col, Row } from "react-bootstrap";
 import WorkRow from "./WorkRow";
 
 function Work() {
-  const testJson = [
-    {
-      id: 42,
-      title: {
-        rendered: "Not Your Summer Google Slides",
-      },
-    },
-    {
-      id: 36,
-      title: {
-        rendered: "Test Client1 &#8211; Test Title1",
-      },
-    },
-    {
-      id: 29,
-      title: {
-        rendered: "Test Workpost1 Title",
-      },
-    },
-    {
-      id: 1,
-      title: {
-        rendered: "Hello world!",
-      },
-    },
-  ];
-  // const [posts, setPosts] = useState(testJson);
-  // loop through the testJSON array, push in arrays of 3 or less
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    Axios.get("https://wpapibbdostudios.azurewebsites.net/wp-json/wp/v2/posts")
+      .then((res) => setPosts(res.data))
+      .catch((err) => console.log(err));
+  }, []);
 
+  // loop through the testJSON array, push in arrays of 3 or less
   function groupPosts(arr) {
     let tempArr = [],
       res = [];
@@ -47,27 +27,7 @@ function Work() {
     return res;
   }
 
-  const groupedPosts = groupPosts(testJson);
-  // let posts = createPostRows(groupedPosts);
-  // [[1,2,3], [4]]
-  //
-  function createPostRows(arr) {
-    arr.map((post) => (
-      <div className="work">
-        testing posts
-        {/* {post.title.rendered} */}
-      </div>
-    ));
-    // const res;
-    // arr.forEach((subArr) => <div className="work-row">{createDivs(subArr)}</div>);
-    arr.forEach((subArr) => {
-      return <div>hi</div>;
-    });
-  }
-
-  // console.log("groupedPosts", groupedPosts);
-  // console.log("GROUPED POSTS", groupedPosts);
-  // console.log("GROUPED POSTS[0]", groupedPosts[1]);
+  const groupedPosts = groupPosts(posts);
 
   return (
     <>
@@ -99,3 +59,30 @@ function Work() {
 }
 
 export default Work;
+
+// const testJson = [
+//   {
+//     id: 42,
+//     title: {
+//       rendered: "Not Your Summer Google Slides",
+//     },
+//   },
+//   {
+//     id: 36,
+//     title: {
+//       rendered: "Test Client1 &#8211; Test Title1",
+//     },
+//   },
+//   {
+//     id: 29,
+//     title: {
+//       rendered: "Test Workpost1 Title",
+//     },
+//   },
+//   {
+//     id: 1,
+//     title: {
+//       rendered: "Hello world!",
+//     },
+//   },
+// ];
