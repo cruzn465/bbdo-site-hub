3742e94073d151b30a268d60a8feb51f3bdb2d30
Nichole <nichole.cruzalegui@bbdo.com>
2023-01-23 12:55:38 -0500

    Fixes bg imgs for each type of row

--- src/components/WorkRow.js
+++ src/components/WorkRow.js
@@ -9,23 +9,23 @@ function WorkRow(props) {
   // MAPS OVER SUBARR & ADDS EMPTY DIVS FOR WHEN THERE'S ONLY 2/3 OR 1/3 IN THE ROW
   // https://wpapibbdostudios.azurewebsites.net/wp-content/uploads/ +  2023/01/1_Thumbnail_AARPKeepingScore.png
   // https://wpapibbdostudios.azurewebsites.net/wp-content/uploads/2023/01/1_Thumbnail_AARPKeepingScore.png"
-  function createDivs(subArr) {
+  function createDivs(postSubArray, mediaSubArray) {
     let emptyDiv = <div className="work-col empty"></div>;
-    if (subArr.length === 3) {
-      return subArr.map((post, i) => (
-        <div className="work-col" key={post.id} style={{ backgroundImage: `url(${"https://wpapibbdostudios.azurewebsites.net" + media[i].source_url})` }}>
+    if (postSubArray.length === 3) {
+      return postSubArray.map((post, i) => (
+        <div className="work-col" key={post.id} style={{ backgroundImage: `url(${"https://wpapibbdostudios.azurewebsites.net" + mediaSubArray[i].source_url})` }}>
           {/* {post.title.rendered} */}
           {/* {post.id} */}
         </div>
       ));
-    } else if (subArr.length === 2) {
+    } else if (postSubArray.length === 2) {
       return (
         <>
-          <div className="work-col" key={subArr[0].id}>
-            {subArr[0].title.rendered}
+          <div className="work-col" key={postSubArray[0].id} style={{ backgroundImage: `url(${"https://wpapibbdostudios.azurewebsites.net" + mediaSubArray[0].source_url})` }}>
+            {/* {postSubArray[0].title.rendered} */}
           </div>
-          <div className="work-col" key={subArr[1].id}>
-            {subArr[1].title.rendered}
+          <div className="work-col" key={postSubArray[1].id} style={{ backgroundImage: `url(${"https://wpapibbdostudios.azurewebsites.net" + mediaSubArray[1].source_url})` }}>
+            {/* {postSubArray[1].title.rendered} */}
           </div>
           {emptyDiv}
         </>
@@ -33,8 +33,8 @@ function WorkRow(props) {
     } else {
       return (
         <>
-          <div className="work-col" key={subArr[0].id}>
-            {subArr[0].title.rendered}
+          <div className="work-col" key={postSubArray[0].id} style={{ backgroundImage: `url(${"https://wpapibbdostudios.azurewebsites.net" + mediaSubArray[0].source_url})` }}>
+            {/* {postSubArray[0].title.rendered} */}
           </div>
           {emptyDiv}
           {emptyDiv}
@@ -42,7 +42,7 @@ function WorkRow(props) {
       );
     }
   }
-  return <div className="work-row">{createDivs(posts)}</div>;
+  return <div className="work-row">{createDivs(posts, media)}</div>;
 }
 
 export default WorkRow;
