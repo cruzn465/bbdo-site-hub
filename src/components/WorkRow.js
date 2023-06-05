14d155d204c1a5e1dfe8e2c5ec421199aedad15c
Nichole <nichole.cruzalegui@bbdo.com>
2023-06-05 14:12:32 -0400

    Adds a small title to the work hovers - only if they exist

--- src/components/WorkRow.js
+++ src/components/WorkRow.js
@@ -24,27 +24,54 @@ function WorkRow(props) {
     return "https://wpapibbdostudios.azurewebsites.net" + source_url;
   }
 
+  // MAKE THIS THE DETERMINE VIDEO MODAL URL fxn
+  // function findSourceUrl(post) {
+  //   // GET A PLACEHOLDER IMAGE FOR POSTS WITHOUT ANY MEDIA
+  //   if (!post.featured_media || !post) return "http://placekitten.com/400/300";
+  //   let ft_media_id = post.featured_media;
+  //   let media = mediaObj[ft_media_id];
+  //   let source_url = media.media_details.sizes.medium_large.source_url;
+  //   return "https://wpapibbdostudios.azurewebsites.net" + source_url;
+  // }
+
+  // decodes and adds a colon
+  function addHoverTitle(str) {
+    let txt = document.createElement("textarea");
+    txt.innerHTML = str + ":";
+    return txt.value;
+  }
+
   function createDivs(postCurArr) {
     let emptyDiv = <div className="work-col empty"></div>;
 
     if (postCurArr.length === 3) {
       return (
         <React.Fragment>
+          {/* clickable outer container */}
           <div
             className="outer-container pointer"
             onClick={(e) =>
               handleClick(e, postCurArr[0], findSourceUrl(postCurArr[0]))
             }
           >
+            {/* bg img */}
             <div
               className="work-col"
               style={{
                 backgroundImage: `url(${findSourceUrl(postCurArr[0])})`,
               }}
             ></div>
+            {/* red overlay with text*/}
             <div className="red abs-cont">
-              <h2 className="post-titles cap">
-                {decode(postCurArr[0].title.rendered)}
+              <h2 className="post-titles">
+                <span className="hover-title">
+                  {postCurArr[0].acf.Client &&
+                    addHoverTitle(postCurArr[0].acf.Client)}
+                </span>
+                <br />
+                <span className="cap">
+                  {decode(postCurArr[0].title.rendered)}
+                </span>
               </h2>
             </div>
           </div>
@@ -61,8 +88,15 @@ function WorkRow(props) {
               }}
             ></div>
             <div className="red abs-cont">
-              <h2 className="post-titles cap">
-                {decode(postCurArr[1].title.rendered)}
+              <h2 className="post-titles">
+                <span className="hover-title">
+                  {postCurArr[1].acf.Client &&
+                    addHoverTitle(postCurArr[1].acf.Client)}
+                </span>
+                <br />
+                <span className="cap">
+                  {decode(postCurArr[1].title.rendered)}
+                </span>
               </h2>
             </div>
           </div>
@@ -79,8 +113,15 @@ function WorkRow(props) {
               }}
             ></div>
             <div className="red abs-cont">
-              <h2 className="post-titles cap">
-                {decode(postCurArr[2].title.rendered)}
+              <h2 className="post-titles">
+                <span className="hover-title">
+                  {postCurArr[2].acf.Client &&
+                    addHoverTitle(postCurArr[2].acf.Client)}
+                </span>
+                <br />
+                <span className="cap">
+                  {decode(postCurArr[2].title.rendered)}
+                </span>
               </h2>
             </div>
           </div>
@@ -102,8 +143,15 @@ function WorkRow(props) {
               }}
             ></div>
             <div className="red abs-cont">
-              <h2 className="post-titles cap">
-                {decode(postCurArr[0].title.rendered)}
+              <h2 className="post-titles">
+                <span className="hover-title">
+                  {postCurArr[0].acf.Client &&
+                    addHoverTitle(postCurArr[0].acf.Client)}
+                </span>
+                <br />
+                <span className="cap">
+                  {decode(postCurArr[0].title.rendered)}
+                </span>
               </h2>
             </div>
           </div>
@@ -121,8 +169,15 @@ function WorkRow(props) {
               }}
             ></div>
             <div className="red abs-cont">
-              <h2 className="post-titles cap">
-                {decode(postCurArr[1].title.rendered)}
+              <h2 className="post-titles">
+                <span className="hover-title">
+                  {postCurArr[1].acf.Client &&
+                    addHoverTitle(postCurArr[1].acf.Client)}
+                </span>
+                <br />
+                <span className="cap">
+                  {decode(postCurArr[1].title.rendered)}
+                </span>
               </h2>
             </div>
           </div>
@@ -148,8 +203,15 @@ function WorkRow(props) {
               }}
             ></div>
             <div className="red abs-cont">
-              <h2 className="post-titles cap">
-                {decode(postCurArr[0].title.rendered)}
+              <h2 className="post-titles">
+                <span className="hover-title">
+                  {postCurArr[0].acf.Client &&
+                    addHoverTitle(postCurArr[0].acf.Client)}
+                </span>
+                <br />
+                <span className="cap">
+                  {decode(postCurArr[0].title.rendered)}
+                </span>
               </h2>
             </div>
           </div>
